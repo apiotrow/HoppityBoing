5cd34366a086b9ad22ee731f0f83b4fea0a14359
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2014-11-13 00:07:52 -0800

    carl is bouncing

---
+++ Assets/CarlBounce.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarlBounce : MonoBehaviour {
+
+	public GameObject trampMesh;
+
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+	
+	}
+	
+
+	void OnTriggerEnter (Collider other)
+	{
+		//if (other.gameObject.name == "TrampolineMesh") {
+		if (other.gameObject == trampMesh) {
+			Debug.Log("hi");
+			rigidbody.velocity = Vector3.zero;
+			rigidbody.AddForce(new Vector3(0f, 100000f, 0f));
+			
+		}
+	}
+
+//	void OnCollisionEnter (Collision other)
+//	{
+//		//if (other.gameObject.name == "TrampolineMesh") {
+//		if (other.gameObject == trampMesh) {
+//			Debug.Log("hi");
+//			rigidbody.velocity = Vector3.zero;
+//			rigidbody.AddForce(new Vector3(0f, 10000f, 0f));
+//			
+//		}
+//	}
+}
